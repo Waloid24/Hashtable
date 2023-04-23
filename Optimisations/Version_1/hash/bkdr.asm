ab2b89c1af7f302d231603f46ee7ca0c5dbe3b13
Andrei Kazachkov <an.kazachkov3234@gmail.com>
2023-04-23 14:09:38 +0300

    Added version 1

---
+++ Optimisations/Version_1/hash/bkdr.asm
@@ -0,0 +1,38 @@
+ global hashBkdr
+
+section .data
+ 
+section .text
+
+hashBkdr:
+   push rdx
+   push rbx
+   push rcx
+
+   xor rax, rax             ; hash
+   mov rbx, 131             ; coefficient
+   xor rcx, rcx
+.loop:
+   movzx rcx, byte [rdi]    ; word
+
+   test rcx, rcx
+   jz .end
+
+   mul rbx
+
+   lea rax, [rax + rcx]
+   inc rdi
+   jmp .loop
+
+.end:
+   and eax, 0x7fffffff
+
+   pop rcx
+   pop rbx
+   pop rdx
+
+   ret
+
+
+
+
---
+++ Optimisations/Version_1/hash/hashfuncs.hpp
@@ -0,0 +1,6 @@
+#ifndef HASHFUNCS_HPP
+#define HASHFUNCS_HPP
+
+extern "C" size_t hashBkdr        (char * word);
+
+#endif
